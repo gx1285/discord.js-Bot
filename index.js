1bcc8d441d88aa3028b8d6cd751b9842bd60d834
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-07-10 14:06:20 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const discord = require("discord.js");
+const app = express();
+const client = new discord.Client({
+  partials: [
+    "USER",
+    "GUILD_MEMBER",
+    "MESSAGE",
+    "REACTION",
+    "GUILD_SCHEDULED_EVENT",
+    "CHANNEL",
+  ],
+  intents: Object.values(discord.Intents.FLAGS),
+});
+const prefix = process.env.prefix;
+const activity = process.env.activity;
+const token = process.env.DISCORD_BOT_TOKEN;
+try {
+  app.get("/", (req, res) => {
+    res.send("Botがオンラインです！");
+  });
+
+  app.listen(8080, () => {});
+
+  client.on("ready", (message) => {
+    console.log("Botが起動したよ～");
+    client.user.setActivity(activity, { type: "PLAYING" });
+  });
+
+  client.on("messageCreate", async (message) => {
+    if (message.author.id == client.user.id || message.author.bot) return;
+    if (message.mentions.has(client.user)) {
+      await message.reply("呼んだ?");
+    }
+    if (!message.content.startsWith(prefix)) return; //ボットのプレフィックスからメッセージが始まっているか確認
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    if (command === "help") {
+      //コマンド名
+      await message.channel.send({
+        embeds: [
+          {
+            title: "ヘルプ",
+            description:
+              "全てのコマンドの初めに`" + prefix + "`をつける必要があります。",
+            fields: [
+              {
+                name: "ヘルプ",
+                value: "`help`",
+              },
+            ],
+          },
+        ],
+      });
+    }
+  });
+  if (token == undefined) {
+    console.log("DISCORD_BOT_TOKENが設定されていません。");
+    process.exit(0);
+  }
+
+  client.login(token);
+} catch (e) {
+  console.log(`エラーが発生しました。\nエラー\n${e}`);
+}
